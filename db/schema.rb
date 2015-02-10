282bd77d309594c89233566046abbcef522feffe
ssomero <sonja.somero@helsinki.fi>
2015-02-10 11:56:33 +0200

    Tests for places

--- db/schema.rb
+++ db/schema.rb
@@ -13,13 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20150130104527) do
 
-  create_table "Memberships", force: :cascade do |t|
-    t.integer  "beer_club_id"
-    t.integer  "user_id"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-  end
-
   create_table "beer_clubs", force: :cascade do |t|
     t.string   "name"
     t.integer  "founded"
@@ -41,6 +34,13 @@ ActiveRecord::Schema.define(version: 20150130104527) do
     t.integer  "year"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "memberships", force: :cascade do |t|
+    t.integer  "beer_club_id"
+    t.integer  "user_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
   create_table "ratings", force: :cascade do |t|
