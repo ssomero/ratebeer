eac82c66a1e99f65977348c80fdb68fb15389855
ssomero <sonja.somero@helsinki.fi>
2015-03-01 18:34:43 +0200

    Breweries/index view modified

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -9,6 +9,18 @@ class BreweriesController < ApplicationController
   def index
     @active_breweries = Brewery.active
     @retired_breweries = Brewery.retired
+
+    order = params[:order] || 'name'
+
+    @active_breweries = case order
+                  when 'name' then @active_breweries.sort_by{ |b| b.name }
+                  when 'year' then @active_breweries.sort_by{ |b| b.year }
+                        end
+
+    @retired_breweries = case order
+                          when 'name' then @retired_breweries.sort_by{ |b| b.name }
+                          when 'year' then @retired_breweries.sort_by{ |b| b.year }
+                        end
   end
 
   # GET /breweries/1
@@ -75,6 +87,8 @@ class BreweriesController < ApplicationController
     def brewery_params
       params.require(:brewery).permit(:name, :year, :active)
     end
+
+
 
 
 
